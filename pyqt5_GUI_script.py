116d3c883a8f6899d720d4cbecf67ba1314266a7
Rahul <rahulgupta6616@gmail.com>
2020-12-29 01:21:39 +0530

    Fixed bugs and added multiple progress bar

--- pyqt5_GUI_script.py
+++ pyqt5_GUI_script.py
@@ -8,6 +8,8 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5 import *
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,6 +17,7 @@ import serial
 import requests
 import time, threading
 import schedule
+from random import randint
 from ws4py.client.threadedclient import WebSocketClient
 
 global ard_data
@@ -33,7 +36,7 @@ class window1(QMainWindow):
 
     def __init__(self):
         super(window1,self).__init__()
-        self.setGeometry(300,100,950,600)        # self -> QMainWindow  ,   
+        self.setGeometry(20,30,1300,650)        # self -> QMainWindow  ,   
         self.setWindowTitle("Simulation Window")      # Title of the main window
         self.UI()
         self.w1 = None
@@ -74,14 +77,13 @@ class window1(QMainWindow):
         #QDialog.createGridLayout()
         #win_lout = QVBoxLayout()
         #win_lout.addWidget(self.horizontalGroupBox)
-
+        
 
 
         self.label1 = QtWidgets.QLabel(self)            # set label on main screen
         self.label2 = QtWidgets.QLabel(self)
         self.label3 = QtWidgets.QLabel(self)            
         self.label4 = QtWidgets.QLabel(self)
-        self.label5 = QtWidgets.QLabel(self)
         self.label6 = QtWidgets.QLabel(self)
         self.label7 = QtWidgets.QLabel(self)
         self.label8 = QtWidgets.QLabel(self)
@@ -92,14 +94,15 @@ class window1(QMainWindow):
 
 
 
-       # self.prog_bar = QtWidgets.QProgressBar(self)     # Added a progress bar
-       # self.prog_bar.setGeometry(200,500,170,22)
+        #self.prog_bar = QtWidgets.QProgressBar(self)     # Added a progress bar
+        #self.prog_bar.setGeometry(200,500,170,22)
        # self.prog_bar.setValue(52)
-       # self.prog_bar.move(200,500)
+        #self.prog_bar.move(200,500)
 
 
         #self.slide = QtWidgets.QSlider(self)
         #self.slide.setGeometry(430,500,190,22)
+        
 
 
 
@@ -107,21 +110,20 @@ class window1(QMainWindow):
         self.label1.setText("Water Level Sensor1")            # set text of label to be printed on main e=screen
         self.label1.move(40,40)                          # set location of label to be placed on main screen as move(x_pos,y_pos)
         self.label2.setText("Water Level Sensor2")            
-        self.label2.move(210,40)
+        self.label2.move(370,40)
         self.label3.setText("Water Level Sensor3")            
-        self.label3.move(380,40)
+        self.label3.move(720,40)
         self.label4.setText("Water Level Sensor4")            
-        self.label4.move(550,40)
-        self.label5.setText("Water Level Sensor5")            
-        self.label5.move(710,40)
+        self.label4.move(1000,40)
+        
         self.label6.setText("MQ2 Sensor1")
-        self.label6.move(40,170)
+        self.label6.move(40,200)
         self.label7.setText("MQ2 Sensor2")
-        self.label7.move(210,170)
+        self.label7.move(370,200)
         self.label8.setText("MQ2 Sensor3")
-        self.label8.move(380,170)
+        self.label8.move(720,200)
         self.label9.setText("MQ2 Sensor4")
-        self.label9.move(550,170)
+        self.label9.move(1000,200)
 
         #with serial.Serial('COM5',9600,timeout=5) as ard_data:
 
@@ -148,48 +150,43 @@ class window1(QMainWindow):
 
         self.push_but2 = QtWidgets.QPushButton(self)
         self.push_but2.setText('Click for graph')
-        self.push_but2.move(205,130)
+        self.push_but2.move(370,130)
         self.push_but2.clicked.connect(self.graph2_box)
 
         self.push_but3 = QtWidgets.QPushButton(self)
         self.push_but3.setText('Click for graph')
-        self.push_but3.move(375,130)
+        self.push_but3.move(720,130)
         self.push_but3.clicked.connect(self.graph3_box)
 
         self.push_but4 = QtWidgets.QPushButton(self)
         self.push_but4.setText('Click for graph')
-        self.push_but4.move(550,130)
+        self.push_but4.move(1000,130)
         self.push_but4.clicked.connect(self.graph4_box)
-
-        self.push_but5 = QtWidgets.QPushButton(self)
-        self.push_but5.setText('Click for graph')
-        self.push_but5.move(710,130)
-        self.push_but5.clicked.connect(self.graph5_box)
 
         self.push_but6 = QtWidgets.QPushButton(self)
         self.push_but6.setText('Click for graph')
-        self.push_but6.move(40,250)
+        self.push_but6.move(40,300)
         self.push_but6.clicked.connect(self.graph6_box)
 
         self.push_but7 = QtWidgets.QPushButton(self)
         self.push_but7.setText('Click for graph')
-        self.push_but7.move(205,250)
+        self.push_but7.move(370,300)
         self.push_but7.clicked.connect(self.graph7_box)
 
         self.push_but8 = QtWidgets.QPushButton(self)
         self.push_but8.setText('Click for graph')
-        self.push_but8.move(375,250)
+        self.push_but8.move(720,300)
         self.push_but8.clicked.connect(self.graph8_box)
 
         self.push_but9 = QtWidgets.QPushButton(self)
         self.push_but9.setText('Click for graph4')
-        self.push_but9.move(550,250)
+        self.push_but9.move(1000,300)
         self.push_but9.clicked.connect(self.graph9_box)
 
-        self.push_but10 = QtWidgets.QPushButton(self)
-        self.push_but10.setText('Click for graph')
-        self.push_but10.move(710,250)
-        self.push_but10.clicked.connect(self.graph10_box)
+       # self.push_but10 = QtWidgets.QPushButton(self)
+       # self.push_but10.setText('Click for graph')
+       # self.push_but10.move(710,250)
+       # self.push_but10.clicked.connect(self.graph10_box)
 
        # self.push_but_r = QtWidgets.QPushButton(self)
        # self.push_but_r.setText('Refresh')
@@ -205,6 +202,62 @@ class window1(QMainWindow):
       #  self.prog_but_res.setText('Reset')
       #  self.prog_but_res.move(300,280)
       #  self.prog_but_res.clicked.connect(self.reset_but)     # For powering OFF the led
+
+
+
+        '''self.graphWidget = pg.PlotWidget()
+        self.setCentralWidget(self.graphWidget)
+
+        self.x = list(range(100))  # 100 time points
+        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
+
+        self.graphWidget.setBackground('w')
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def update_plot_data(self):
+
+        self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+        self.y = self.y[1:]  # Remove the first 
+        self.y.append(randint(0,100))  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)  # Update the data.
+        '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
       #  self.button1 = QtWidgets.QPushButton(self)
@@ -247,46 +300,65 @@ class window1(QMainWindow):
         self.read = QPushButton("Read", self)
         self.read.clicked.connect(self.read_data)
         self.read.setFixedSize(100, 40)
-        self.read.move(390, 390)
+        self.read.move(400, 490)
 
         self.clear = QPushButton("Clear", self)
         self.clear.clicked.connect(self.clear_data)
         self.clear.setFixedSize(100, 40)
-        self.clear.move(240, 390)
+        self.clear.move(540, 490)
 
         self.cont =QCheckBox(self)
         self.cont.stateChanged.connect(self.up_1s)
-        self.cont.move(352,370)
+        self.cont.move(500,400)
 
         self.label14 = QLabel("Water meter 1", self)
         setFnt = QFont("Times", 9, QFont.Bold)
         self.label14.setFont(setFnt)
-        self.label14.setFixedSize(300, 35)
-        self.label14.move(50,80)
+        self.label14.setFixedSize(300, 45)
+        self.label14.move(40,80)
 
         self.label15 = QLabel("Water meter 2", self)
         setFnt = QFont("Times", 9, QFont.Bold)
         self.label15.setFont(setFnt)
-        self.label15.setFixedSize(300, 35)
-        self.label15.move(210,80)
+        self.label15.setFixedSize(300, 45)
+        self.label15.move(370,80)
 
         self.label16 = QLabel("Water meter 3", self)
         setFnt = QFont("Times", 9, QFont.Bold)
         self.label16.setFont(setFnt)
-        self.label16.setFixedSize(300, 35)
-        self.label16.move(380,80)
+        self.label16.setFixedSize(300, 45)
+        self.label16.move(720,80)
 
         self.label17 = QLabel("Water meter 4", self)
         setFnt = QFont("Times", 9, QFont.Bold)
         self.label17.setFont(setFnt)
-        self.label17.setFixedSize(300, 35)
-        self.label17.move(550,80)
+        self.label17.setFixedSize(300, 45)
+        self.label17.move(1000,80)
 
-        self.label18 = QLabel("Water meter 5", self)
+        self.label19 = QLabel("MQ2 sensor 1", self)
         setFnt = QFont("Times", 9, QFont.Bold)
-        self.label18.setFont(setFnt)
-        self.label18.setFixedSize(300, 35)
-        self.label18.move(710,80)
+        self.label19.setFont(setFnt)
+        self.label19.setFixedSize(300, 35)
+        self.label19.move(40,240)
+
+        self.label20 = QLabel("MQ2 sensor 2", self)
+        setFnt = QFont("Times", 9, QFont.Bold)
+        self.label20.setFont(setFnt)
+        self.label20.setFixedSize(300, 35)
+        self.label20.move(370,240)
+
+        self.label21 = QLabel("MQ2 sensor 3", self)
+        setFnt = QFont("Times", 9, QFont.Bold)
+        self.label21.setFont(setFnt)
+        self.label21.setFixedSize(300, 35)
+        self.label21.move(720,240)
+
+        self.label22 = QLabel("MQ2 sensor 4", self)
+        setFnt = QFont("Times", 9, QFont.Bold)
+        self.label22.setFont(setFnt)
+        self.label22.setFixedSize(300, 35)
+        self.label22.move(1000,240)
+
 
 
         self.TimerTask=None
@@ -302,19 +374,87 @@ class window1(QMainWindow):
 
 
 
+    #global win
+    
+   # win = pg.plot()
+    #x = np.arange(1)
+        self.prog_bar1 = QtWidgets.QProgressBar(self)     # Added a progress bar
+        self.prog_bar1.setGeometry(200,500,170,52)
+        self.prog_bar1.move(170,107)
+
+        self.prog_bar2 = QtWidgets.QProgressBar(self)     # Added a progress bar
+        self.prog_bar2.setGeometry(200,500,170,52)
+        self.prog_bar2.move(500,107)
+
+        self.prog_bar3 = QtWidgets.QProgressBar(self)     # Added a progress bar
+        self.prog_bar3.setGeometry(200,500,170,52)
+        self.prog_bar3.move(830,107)
+
+        self.prog_bar4 = QtWidgets.QProgressBar(self)     # Added a progress bar
+        self.prog_bar4.setGeometry(200,500,170,52)
+        self.prog_bar4.move(1130,107)
+
+        self.prog_bar6 = QtWidgets.QProgressBar(self)     # Added a progress bar
+        self.prog_bar6.setGeometry(200,500,170,52)
+        self.prog_bar6.move(170,280)
+
+        self.prog_bar7 = QtWidgets.QProgressBar(self)     # Added a progress bar
+        self.prog_bar7.setGeometry(200,500,170,52)
+        self.prog_bar7.move(500,280)
+
+        self.prog_bar8 = QtWidgets.QProgressBar(self)     # Added a progress bar
+        self.prog_bar8.setGeometry(200,500,170,52)
+        self.prog_bar8.move(830,280)
+
+        self.prog_bar9 = QtWidgets.QProgressBar(self)     # Added a progress bar
+        self.prog_bar9.setGeometry(200,500,170,52)
+        self.prog_bar9.move(1130,280)
+
 
 
 
     def read_data(self):
+        global k
         k = ard_data.readline().decode()
         self.label14.setText(k)
-        print(k)
+        self.prog_bar1.setValue(int(k))
+        
         k = ard_data.readline().decode()
         self.label15.setText(k)
-        #print(k)
+        self.prog_bar2.setValue(int(k))
+
+        
+        k = ard_data.readline().decode()
+        self.label16.setText(k)
+        self.prog_bar3.setValue(int(k))
+        k = ard_data.readline().decode()
+        self.label17.setText(k)
+        self.prog_bar4.setValue(int(k))
+
+        k = ard_data.readline().decode()
+        self.label19.setText(k)
+        self.prog_bar6.setValue(int(k))
+
+        k = ard_data.readline().decode()
+        self.label20.setText(k)
+        self.prog_bar7.setValue(int(k))
+
+        k = ard_data.readline().decode()
+        self.label21.setText(k)
+        self.prog_bar8.setValue(int(k))
+
+        #k = ard_data.readline().decode()
+        #self.label22.setText(k)
+        #return(k)
 
     def clear_data(self):
         self.label14.clear()
+        self.label15.clear()
+        self.label16.clear()
+        self.label17.clear()
+        self.label19.clear()
+        self.label20.clear()
+        self.label21.clear()
 
 
     def up_1s(self,state):
@@ -329,11 +469,8 @@ class window1(QMainWindow):
 
 
             if state == QtCore.Qt.Checked:
-                #self.read()
                 self.TimerTask.timeout.connect(self.read_data)
-            #    print(self.TimerTask.timeout.connect(self.read_data))
                 self.TimerTask.start(1000)
-                
             else:
                 self.TimerTask.stop()
                 
@@ -419,10 +556,6 @@ class window1(QMainWindow):
             self.w4 = another_window4()
         self.w4.show()
 
-    def graph5_box(self, checeked):
-        if(self.w5==None):
-            self.w5 = another_window5()
-        self.w5.show()
 
     def graph6_box(self, checeked):
         if(self.w6==None):
@@ -451,15 +584,18 @@ class window1(QMainWindow):
 
 
 
-class another_window1(QWidget):
+class another_window1(QMainWindow):
 
     def __init__(self):
-        super().__init__()
+        super(another_window1,self).__init__()
+        #super().__init__()
         layout = QVBoxLayout()
-        self.setGeometry(200,200,400,600)
+        self.setGeometry(200,200,500,300)
         self.label = QLabel("Graph window")
         layout.addWidget(self.label)
-        self.setLayout(layout) 
+        self.setLayout(layout)
+
+    
 
 class another_window2(QWidget):
 
--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,18 +1,29 @@
 const int trigPin1 = 9;
 const int trigPin2 = 3;
+const int ir1 = A0;
+const int ir2 = A1;
+const int ir3 = A2;
+const int ir4 = A3;
+const int ir5 = A4;
 
 const int echoPin1 = 10;
-const int echoPin2 = 11;
+const int echoPin2 = 6;
 // defines variables
 long duration1;
 long duration2;
-
+int ir;
+int ir21;
+int ir31;
+int ir41;
+int ir51;
 int distance1;
 int distance2;
 
 void setup() {
 pinMode(trigPin1, OUTPUT);// Sets the trigPin as an Output
 pinMode(trigPin2, OUTPUT);
+pinMode(ir1,INPUT);
+pinMode(ir2,INPUT);
 pinMode(echoPin1, INPUT); // Sets the echoPin as an Input
 pinMode(echoPin2, INPUT);
 Serial.begin(9600); // Starts the serial communication
@@ -22,10 +33,12 @@ void loop() {
 //  {
 // Clears the trigPin
 digitalWrite(trigPin1, LOW);
+delayMicroseconds(2);
 digitalWrite(trigPin2,LOW);
 delayMicroseconds(2);
 // Sets the trigPin on HIGH state for 10 micro seconds
 digitalWrite(trigPin1, HIGH);
+delayMicroseconds(10);
 digitalWrite(trigPin2,HIGH);
 delayMicroseconds(10);
 digitalWrite(trigPin1, LOW);
@@ -33,6 +46,11 @@ digitalWrite(trigPin2,LOW);
 // Reads the echoPin, returns the sound wave travel time in microseconds
 duration1 = pulseIn(echoPin1, HIGH);
 duration2 = pulseIn(echoPin2,HIGH);
+ir = analogRead(ir1);
+ir21 = analogRead(ir2);
+ir31 = analogRead(ir3);
+ir41 = analogRead(ir4);
+ir51 = analogRead(ir5);
 // Calculating the distance
 distance1 = duration1*0.034/2;
 distance2 = duration2*0.034/2;
@@ -41,10 +59,19 @@ distance2 = duration2*0.034/2;
 //Serial.print("Distance: ");
 //String dataread = String((distance/1024)*5);
 Serial.println(distance1);
-delay(2000);
-Serial.println(distance2);
-
-delay(2000);
+delay(1000);
+Serial.println(duration2);
+delay(1000);
+Serial.println(ir);
+delay(1000);
+Serial.println(ir21);
+delay(1000);
+Serial.println(ir31);
+delay(1000);
+Serial.println(ir41);
+delay(1000);
+Serial.println(ir51);
+delay(1000);
 //Serial.write(distance);
   }
 //}
